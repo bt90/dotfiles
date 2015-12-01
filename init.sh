77af248f736df52be0d3cc8df294b0e7250f83e8
Thomas Butz <btom1990@googlemail.com>
2015-12-01 14:02:44 +0100

    Fixed init.sh and changed some options for pacman and makepkg

--- init.sh
+++ init.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Creates symlinks to the config files in the current directory using GNU stow
 
-# Check for root priviliges
-if [[ $EUID -ne 0 ]]; then
-   printf "Please run as root:\nsudo %s\n" "${0}"
-   exit 1
-fi
-
 # Check if stow is installed
 if [[ (! -x "/sbin/stow") ]]; then
     echo "Please install stow"
@@ -18,5 +12,5 @@ cd $(dirname $0)
 
 stow git
 stow ssh
-sudo -t / makepgk
-sudo -t / pacman
+sudo stow -t / makepgk
+sudo stow -t / pacman
